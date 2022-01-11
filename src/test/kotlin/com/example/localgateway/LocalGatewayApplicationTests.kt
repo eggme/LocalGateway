633c7feda932a664daf9f7d50831c58524f218b3
eggme <kyyeto9984@naver.com>
2022-01-11 16:01:14 +0900

    Initial commit

---
+++ src/test/kotlin/com/example/localgateway/LocalGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.localgateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LocalGatewayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
